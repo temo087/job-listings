938280c5b53fabcefe60bbe1600ff0ffd7d97d61
temo087 <lizii7724@gmail.com>
2024-02-29 22:54:47 +0400

    job-listings

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center p-10">
+
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Background.tsx
@@ -0,0 +1,13 @@
+
+
+const Background = () => {
+  return (
+    <img
+      src="/assets/images/bg-header-desktop.svg"
+      alt=""
+      className="h-full w-full flex items-center justify-center"
+    />
+  );
+};
+
+export default Background;
